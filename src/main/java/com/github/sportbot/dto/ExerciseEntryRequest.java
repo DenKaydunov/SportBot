de0a40ca8eaf0189846a4697deed99dbf6bc3818
DenKaydunov <deniskaydunov@gmail.com>
2025-07-20 22:41:35 +0300

    TSP-124-exercises (#9)
    * fix lombok and java version
    therefore spring don't support 24 version yet
    * fill exercise_types table
    * implement exercises functionality
    * add swagger
    * add test data sql query

---
+++ src/main/java/com/github/sportbot/dto/ExerciseEntryRequest.java
@@ -0,0 +1,10 @@
+package com.github.sportbot.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ExerciseEntryRequest(
+        @NotNull Long telegramId,
+        @NotBlank String exerciseType,
+        int count
+) {}
---
+++ src/main/java/com/github/sportbot/service/ExerciseService.java
@@ -0,0 +1,38 @@
+package com.github.sportbot.service;
+
+import com.github.sportbot.dto.ExerciseEntryRequest;
+import com.github.sportbot.exception.DaoException;
+import com.github.sportbot.model.*;
+import com.github.sportbot.repository.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class ExerciseService {
+
+    private final UserRepository userRepository;
+    private final ExerciseTypeRepository exerciseTypeRepository;
+
+    @Transactional
+    public void saveExerciseEntry(ExerciseEntryRequest req) {
+        User user = userRepository.findByTelegramId(req.telegramId())
+                .orElseThrow(() -> new DaoException("User not found"));
+
+        ExerciseType exerciseType = exerciseTypeRepository.findByCode(req.exerciseType())
+                .orElseThrow(() -> new DaoException("Unknown exercise code"));
+
+        WorkoutHistory exercise = WorkoutHistory.builder()
+                .user(user)
+                .exerciseType(exerciseType)
+                .count(req.count())
+                .date(LocalDate.now())
+                .build();
+
+        user.getWorkoutHistory().add(exercise);
+        userRepository.save(user);
+    }
+}
