5454f1a98c59ed8f7f148fdf68ad9a3fad54f377
Denis Kaydunov <deniskaydunov@gmail.com>
2025-07-20 22:59:44 +0300

    code cleaning

--- src/main/java/com/github/sportbot/dto/ExerciseEntryRequest.java
+++ src/main/java/com/github/sportbot/dto/ExerciseEntryRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record ExerciseEntryRequest(
-        @NotNull Long telegramId,
+        @NotNull Integer telegramId,
         @NotBlank String exerciseType,
         int count
 ) {}
