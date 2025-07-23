df627c13dd81b5005d933608e652d048f86360d7
Denis Kaydunov <deniskaydunov@gmail.com>
2025-07-23 18:05:50 +0300

    prepared test environment

--- src/test/resources/changelog/changes/data.sql
+++ src/test/resources/changelog/changes/data.sql
@@ -8,24 +8,21 @@
 -- =========================================
 INSERT INTO users (telegram_id, is_subscribed, created_at, updated_at)
 VALUES
-    ( 123456789, TRUE, NOW(), NOW())
-ON CONFLICT (id) DO NOTHING;
+    (123456789, TRUE, NOW(), NOW());
 
 -- =========================================
 -- ⚙️ 2. Добавить профиль к пользователю
 -- =========================================
-INSERT INTO user_profiles ( user_id, full_name, remind_time)
+INSERT INTO user_profiles (user_id, full_name, remind_time)
 VALUES
-    ( 1, 'Юра', '09:00:00')
-ON CONFLICT (id) DO NOTHING;
+    (1, 'Test User', '09:00:00');
 
 -- =========================================
 -- ⚙️ 3. Пример истории тренировок
 -- (два подхода по 25 отжиманий за сегодня)
 -- =========================================
-INSERT INTO workout_history ( user_id, exercise_type_id, count, date)
+INSERT INTO workout_history (user_id, exercise_type_id, count, date)
 VALUES
     ( 1, 1, 25, CURRENT_DATE),
-    ( 1, 1, 25, CURRENT_DATE)
-ON CONFLICT (id) DO NOTHING;
+    (1, 1, 30, CURRENT_DATE);
 
