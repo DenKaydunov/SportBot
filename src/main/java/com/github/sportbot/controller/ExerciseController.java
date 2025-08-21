d8d9e3c156ba5223b0072d636adec06468efe74c
VelichkoStepan1 <st.greatov1@yandex.ru>
2025-08-21 20:11:31 +0700

    TSP-175  Изменен контроллер

--- src/main/java/com/github/sportbot/controller/ExerciseController.java
+++ src/main/java/com/github/sportbot/controller/ExerciseController.java
@@ -1,7 +1,9 @@
 package com.github.sportbot.controller;
 
+import com.github.sportbot.constants.MessageConstants;
 import com.github.sportbot.dto.ExerciseEntryRequest;
 import com.github.sportbot.service.ExerciseService;
+import jakarta.validation.MessageInterpolator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,12 +23,14 @@ public class ExerciseController {
     }
 
     @PostMapping
-    public void saveEntry(@RequestBody @Valid ExerciseEntryRequest req) {
+    public String saveEntry(@RequestBody @Valid ExerciseEntryRequest req) {
         exerciseService.saveExerciseEntry(req);
+        return MessageConstants.EXERCISE_RECORDED;
     }
 
     @PostMapping("/max")
-    public void saveMax(@RequestBody @Valid ExerciseEntryRequest req) {
+    public String saveMax(@RequestBody @Valid ExerciseEntryRequest req) {
         exerciseService.saveMaxEntry(req);
+        return MessageConstants.MAX_EXERCISE_RECORDED;
     }
 }
