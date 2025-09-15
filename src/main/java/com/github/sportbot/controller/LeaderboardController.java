0394b259d0f4122dedd15c4c8ab4320291d83ac9
DenKaydunov <deniskaydunov@gmail.com>
2025-09-15 16:38:35 +0300

    renamed urls (#33)

--- src/main/java/com/github/sportbot/controller/LeaderboardController.java
+++ src/main/java/com/github/sportbot/controller/LeaderboardController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/leaderboard")
+@RequestMapping("/api/v1/leaderboard")
 @RequiredArgsConstructor
 public class LeaderboardController {
 
--- src/main/java/com/github/sportbot/controller/SquatsController.java
+++ src/main/java/com/github/sportbot/controller/SquatsController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/squats")
+@RequestMapping("/api/v1/squats")
 public class SquatsController {
 
 
