eb441e202b76eea0d54b9ddfcbcf97112944b62d
Denis Kaydunov <deniskaydunov@gmail.com>
2025-10-13 18:41:00 +0500

    deleted unnecessary method

--- src/main/java/com/github/sportbot/model/ExerciseTypeEnum.java
+++ src/main/java/com/github/sportbot/model/ExerciseTypeEnum.java
@@ -11,13 +11,4 @@ public enum ExerciseTypeEnum {
     SQUAT("squat");
 
     private final String type;
-
-    public static ExerciseTypeEnum fromCode(String code) {
-        for (ExerciseTypeEnum exerciseCode : values()) {
-            if (exerciseCode.type.equalsIgnoreCase(code)) {
-                return exerciseCode;
-            }
-        }
-        throw new IllegalArgumentException("Unknown exercise code: " + code);
-    }
 }
