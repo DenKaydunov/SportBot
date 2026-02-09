17e1f7d54e867c2eb10f63b219ca8068f7b54048
DenKaydunov <deniskaydunov@gmail.com>
2026-02-09 10:30:35 +0500

    Add ABS (#58)
    fixed error about enum.
    Enum is Deprecated

--- src/main/java/com/github/sportbot/model/ExerciseTypeEnum.java
+++ src/main/java/com/github/sportbot/model/ExerciseTypeEnum.java
@@ -1,24 +1,18 @@
 package com.github.sportbot.model;
 
-import com.github.sportbot.exception.UnknownExerciseCodeException;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
-
+/**
+ * @deprecated it looks unuseful, but it used in the tests
+ */
 @Getter
 @RequiredArgsConstructor
+@Deprecated(forRemoval = true)
 public enum ExerciseTypeEnum {
     PUSH_UP("push_up"),
     PULL_UP("pull_up"),
     SQUAT("squat");
 
     private final String type;
-
-    public static ExerciseTypeEnum getExerciseType(String code) {
-        return Arrays.stream(values())
-                .filter(e -> e.type.equalsIgnoreCase(code))
-                .findFirst()
-                .orElseThrow(() -> new UnknownExerciseCodeException(code));
-    }
 }
