7c03e3a02aed8a894c75442522c355fbcbc7eb9a
Denis Kaydunov <deniskaydunov@gmail.com>
2026-03-19 17:02:24 +0300

    TSP-385: implement ranks for all types of exercises

--- src/main/resources/db/changelog/changes/0015-remove-old-ranks-and-make-global.sql
+++ src/main/resources/db/changelog/changes/0015-remove-old-ranks-and-make-global.sql
@@ -1,12 +1,11 @@
 --liquibase formatted sql
 
---changeset deniskaydunov:15
--- Удаляем user_ranks, связанные со старыми pull_up рангами
-DELETE FROM user_ranks
-WHERE rank_id IN (SELECT id FROM ranks WHERE exercise_type_id = 2);
+--changeset deniskaydunov:15-fix
+-- Удаляем все user_ranks (связи пользователей со старыми рангами)
+DELETE FROM user_ranks;
 
--- Удаляем старые 30 рангов для pull_up
-DELETE FROM ranks WHERE exercise_type_id = 2;
+-- Удаляем все старые ранги
+DELETE FROM ranks;
 
 -- Делаем exercise_type_id nullable для общих рангов
 ALTER TABLE ranks ALTER COLUMN exercise_type_id DROP NOT NULL;
